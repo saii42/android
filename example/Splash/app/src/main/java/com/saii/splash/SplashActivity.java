b8bf71208af9969ea6224b100c1044902c894217
saii <onad94@gmail.com>
2022-04-28 14:43:19 +0900

    saii
    splash

---
+++ example/Splash/app/src/main/java/com/saii/splash/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.saii.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        moveMain(1);
+
+    }
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000 * sec);
+    }
+}
