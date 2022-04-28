9111d09b9aaa1bb1124adac72decac5f38f2b562
saii <onad94@gmail.com>
2022-04-28 16:34:58 +0900

    saii
    Intro - ViewPager2를 이용한 가이드 화면

---
+++ example/ViewPager2_Guide/app/src/main/java/com/saii/viewpager2_guide/MainActivity.java
@@ -0,0 +1,14 @@
+package com.saii.viewpager2_guide;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
