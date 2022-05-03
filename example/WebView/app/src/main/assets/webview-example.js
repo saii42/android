23306a4fe71431bed05aa19359ccf4b11937b2f6
saii <onad94@gmail.com>
2022-05-03 15:25:26 +0900

    WebView1
    WebView1

---
+++ example/WebView/app/src/main/assets/webview-example.js
@@ -0,0 +1,3 @@
+    (function() {
+    document.getElementById("webview-example").style.fontStyle = "italic";
+}());
--- example/WebView/app/src/main/java/com/saii/webview/MainActivity.java
+++ example/WebView/app/src/main/java/com/saii/webview/MainActivity.java
@@ -1,14 +1,28 @@
 package com.saii.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.wv_main);
+
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        mWebView.setWebViewClient(new WebViewClient(){
+        });
+        //mWebView.loadUrl("https://saii42.tistory.com/97");
+        mWebView.loadUrl("file:///android_asset/index.html");
+
     }
 }
