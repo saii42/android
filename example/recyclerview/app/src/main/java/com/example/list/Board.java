fb07e9a38f43b195071db531323abdef50fd3886
saii <onad94@gmail.com>
2022-08-29 04:47:23 +0900

    movie

---
+++ example/recyclerview/app/src/main/java/com/example/list/Board.java
@@ -0,0 +1,49 @@
+package com.example.list;
+
+
+public class Board {
+
+   private String title;
+   private String contents;
+   private String time;
+   private String writer;
+
+   public Board(String title, String contents, String time, String writer) {
+      this.title = title;
+      this.contents = contents;
+      this.time = time;
+      this.writer = writer;
+   }
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getContents() {
+      return contents;
+   }
+
+   public void setContents(String contents) {
+      this.contents = contents;
+   }
+
+   public String getTime() {
+      return time;
+   }
+
+   public void setTime(String time) {
+      this.time = time;
+   }
+
+   public String getWriter() {
+      return writer;
+   }
+
+   public void setWriter(String writer) {
+      this.writer = writer;
+   }
+}
